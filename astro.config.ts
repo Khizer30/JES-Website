bb9b7ea0dd28d12b10d5a0d49debdee3f15e358d
Syed Muhammad Khizer <syed.khizer30@gmail.com>
2022-08-22 15:14:27 +0500

    v0.2
    *About Us Added

--- astro.config.ts
+++ astro.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "astro/config" ;
 import prefetch from "@astrojs/prefetch" ;
+import compress from "astro-compress" ;
 
-export default defineConfig({ integrations: [prefetch()] }) ;
+export default defineConfig({ integrations: [prefetch(), compress()] }) ;
