6e07eaf230ed4001e93d88414aea7633cb8fc4f9
Syed Muhammad Khizer <syed.khizer30@gmail.com>
2022-08-25 14:35:11 +0500

    v0.95
    *Client Only Used

--- src/components/Adopt.tsx
+++ src/components/Adopt.tsx
@@ -17,6 +17,7 @@ function Adopt(): JSX.Element
 {
   // Variables
   const [inputs, setInputs] = useState<Inputs>({ name: "", email: "", contact: "", children: "", message: "" }) ;
+  const form: HTMLFormElement = document.getElementById("theForm")! ;
 
   // Send Email
   function send(): void
@@ -24,7 +25,7 @@ function Adopt(): JSX.Element
     if (checkIt(inputs.name, 30) && checkIt(inputs.email, 50) &&
     (inputs.contact, 30) && checkIt(inputs.children, 3) && checkIt(inputs.message, 500))
     {
-      emailjs.send("service_lbk0op9", "jes_adoption", inputs, "n1WTOESB7GzuR5U_a")
+      emailjs.sendForm("service_lbk0op9", "jes_adoption", form, "n1WTOESB7GzuR5U_a")
       .then(() => alert("Form Submitted! We Will Contact You Shortly!"))
       .catch(() => alert("Error Submitting The Form! Please Try Later!")) ;
     }
@@ -55,34 +56,29 @@ function Adopt(): JSX.Element
 
   return (
   <>
-    <form method="post" target="_self" 
-    encType="multipart/form-data" autoComplete="off" className="width85" noValidate>
+    <input name="name" type="text" maxLength={ 30 } required
+    placeholder="Name*" className="form-control franchiseTxt" autoFocus
+    value={ inputs.name } onChange={ handleChange } />
+    
+    <input name="email" type="email" maxLength={ 50 } required
+    placeholder="Email*" className="form-control franchiseTxt"
+    value={ inputs.email } onChange={ handleChange } />
 
-      <input name="name" type="text" maxLength={ 30 } required
-      placeholder="Name*" className="form-control franchiseTxt" autoFocus
-      value={ inputs.name } onChange={ handleChange } />
-      
-      <input name="email" type="email" maxLength={ 50 } required
-      placeholder="Email*" className="form-control franchiseTxt"
-      value={ inputs.email } onChange={ handleChange } />
+    <input name="contact" type="text" maxLength={ 30 } required
+    placeholder="Contact*" className="form-control franchiseTxt"
+    value={ inputs.contact } onChange={ handleChange } />
 
-      <input name="contact" type="text" maxLength={ 30 } required
-      placeholder="Contact*" className="form-control franchiseTxt"
-      value={ inputs.contact } onChange={ handleChange } />
+    <input name="children" type="number" max={ 10 } min={ 1 } required
+    placeholder="No. of Children*" className="form-control franchiseTxt"
+    value={ inputs.children } onChange={ handleChange } />
 
-      <input name="children" type="number" max={ 10 } min={ 1 } required
-      placeholder="No. of Children*" className="form-control franchiseTxt"
-      value={ inputs.children } onChange={ handleChange } />
+    <textarea name="message" maxLength={ 500 } wrap="off" required
+    placeholder="Message*"  className="form-control franchiseTxtArea"
+    value={ inputs.message } onChange={ handleChange }></textarea>
 
-      <textarea name="message" maxLength={ 500 } wrap="off" required
-      placeholder="Message*"  className="form-control franchiseTxtArea"
-      value={ inputs.message } onChange={ handleChange }></textarea>
-
-      <div className="text-center">
-        <button type="button" onClick={ send } className="franchiseBtn2"> Submit </button>
-      </div>
-      
-    </form>
+    <div className="text-center">
+      <button type="button" onClick={ send } className="franchiseBtn2"> Submit </button>
+    </div>
   </>
   )
 }
--- src/components/Franchise.tsx
+++ src/components/Franchise.tsx
@@ -17,6 +17,7 @@ function Franchise(): JSX.Element
 {
   // Variables
   const [inputs, setInputs] = useState<Inputs>({ name: "", email: "", contact: "", subject: "", message: "" }) ;
+  const form: HTMLFormElement = document.getElementById("theForm")! ;
 
   // Send Email
   function send(): void
@@ -24,7 +25,7 @@ function Franchise(): JSX.Element
     if (checkIt(inputs.name, 30) && checkIt(inputs.email, 50) &&
     (inputs.contact, 30) && checkIt(inputs.subject, 30) && checkIt(inputs.message, 500))
     {
-      emailjs.send("service_lbk0op9", "jes_franchise", inputs, "n1WTOESB7GzuR5U_a")
+      emailjs.sendForm("service_lbk0op9", "jes_franchise", form, "n1WTOESB7GzuR5U_a")
       .then(() => alert("Form Submitted! We Will Contact You Shortly!"))
       .catch(() => alert("Error Submitting The Form! Please Try Later!")) ;
     }
@@ -55,34 +56,29 @@ function Franchise(): JSX.Element
 
   return (
   <>
-    <form method="post" target="_self" 
-    encType="multipart/form-data" autoComplete="off" className="width85" noValidate>
+    <input name="name" type="text" maxLength={ 30 } required
+    placeholder="Name*" className="form-control franchiseTxt" autoFocus
+    value={ inputs.name } onChange={ handleChange } />
+    
+    <input name="email" type="email" maxLength={ 50 } required
+    placeholder="Email*" className="form-control franchiseTxt"
+    value={ inputs.email } onChange={ handleChange } />
 
-      <input name="name" type="text" maxLength={ 30 } required
-      placeholder="Name*" className="form-control franchiseTxt" autoFocus
-      value={ inputs.name } onChange={ handleChange } />
-      
-      <input name="email" type="email" maxLength={ 50 } required
-      placeholder="Email*" className="form-control franchiseTxt"
-      value={ inputs.email } onChange={ handleChange } />
+    <input name="contact" type="text" maxLength={ 30 } required
+    placeholder="Contact*" className="form-control franchiseTxt"
+    value={ inputs.contact } onChange={ handleChange } />
 
-      <input name="contact" type="text" maxLength={ 30 } required
-      placeholder="Contact*" className="form-control franchiseTxt"
-      value={ inputs.contact } onChange={ handleChange } />
+    <input name="subject" type="text" maxLength={ 30 } required
+    placeholder="Subject*" className="form-control franchiseTxt"
+    value={ inputs.subject } onChange={ handleChange } />
 
-      <input name="subject" type="text" maxLength={ 30 } required
-      placeholder="Subject*" className="form-control franchiseTxt"
-      value={ inputs.subject } onChange={ handleChange } />
+    <textarea name="message" maxLength={ 500 } wrap="off" required
+    placeholder="Message*"  className="form-control franchiseTxtArea"
+    value={ inputs.message } onChange={ handleChange }></textarea>
 
-      <textarea name="message" maxLength={ 500 } wrap="off" required
-      placeholder="Message*"  className="form-control franchiseTxtArea"
-      value={ inputs.message } onChange={ handleChange }></textarea>
-
-      <div className="text-center">
-        <button type="button" onClick={ send } className="franchiseBtn2"> Submit </button>
-      </div>
-      
-    </form>
+    <div className="text-center">
+      <button type="button" onClick={ send } className="franchiseBtn2"> Submit </button>
+    </div>
   </>
   )
 }
